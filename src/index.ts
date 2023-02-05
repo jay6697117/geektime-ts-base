f8583740b417f482f86e75bca760a9a04c0a45f0
steve zhang <871506263@qq.com>
2023-02-05 22:28:27 +0800

    zhangjinhui提交代码于: 2023-02-05 22:28:27

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,1 @@
-// import './datatype';
-export default {};
-
-const str: string = 'hello world 666';
-const dom = document.getElementById('root') as HTMLElement;
-dom.innerHTML = str;
+import './datatype';
